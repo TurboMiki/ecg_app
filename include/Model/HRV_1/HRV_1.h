f146e293a1ae8f4e6f4bea6c92c615ab064b333e
Oliwia <oliwia.salamon21@gmail.com>
2024-12-07 20:23:10 +0100

    HRV_1 time params part 3

--- include/Model/HRV_1/HRV_1.h
+++ include/Model/HRV_1/HRV_1.h
@@ -6,14 +6,16 @@
 class HRV_1 {
 public:
     void process(const Signal& rpeaks, const Signal& ecg);
-    std::array<float, 5> getTimeParams() const;
-    std::array<float, 6> getFreqParams() const;
+    std::array<double, 5> getTimeParams() const;
+    std::array<double, 6> getFreqParams() const;
 
 private:
+    std::vector<double> getIntervals(const Signal& rpeaks) const;
+    double calculateRRMean(const std::vector<double>& nnIntervals) const;
     double calculateRMSSD(const std::vector<double>& nnIntervals) const;
     double calculateSDNN(const std::vector<double>& nnIntervals) const;
     double calculateNN50(const std::vector<double>& nnIntervals) const;
     double calculatepNN50(const std::vector<double>& nnIntervals, double nn50) const;
-    std::array<float, 5> timeParams_;
-    std::array<float, 6> freqParams_;
+    std::array<double, 5> timeParams_;
+    std::array<double, 6> freqParams_;
 };
--- src/Model/HRV_1/HRV_1.cpp
+++ src/Model/HRV_1/HRV_1.cpp
@@ -3,37 +3,20 @@
 
 // HRV 1 process function
 void HRV_1::process(const Signal& rpeaks, const Signal& ecg){
-    // zmienne
+    // variables
     double meanRR = 0.0;
     double sdnn = 0.0;
     double rmssd = 0.0;
     double nn50 = 0.0;
     double pnn50 = 0.0;
-    std::vector<double> nnIntervals;
 
     // time parameters
-
-    // 1. RR_mean - rpeaks mean
-    for (size_t i = 0; i < rpeaks.size() - 1; i++){
-        meanRR += rpeaks[i + 1] - rpeaks[i];
-        nnIntervals.push_back(rpeaks[i + 1] - rpeaks[i]); // Creating intervals for SDNN
-
-    }
-    meanRR /= (rpeaks.size()-1);
-
-    //2. SDNN - standard deviation for RR distances
-    sdnn = calculateSDNN(nnIntervals);
-
-    //3. RMSSD - root mean sqare from RR intervals distance
-    rmssd = calculateRMSSD(nnIntervals);
-
-    //4. NN50  - Pair of neighbours RR with more than 50 distance
-    nn50 = calculateNN50(nnIntervals);
-
-    //5. pNN50 - percent of NN50 compared to all RR intervals
-    pnn50 = calculatepNN50(nnIntervals, nn50);
-
-
+    std::vector<double> nnIntervals = getIntervals(rpeaks);
+    meanRR = calculateRRMean(nnIntervals);          // 1. RR_mean - rpeaks mean
+    sdnn = calculateSDNN(nnIntervals);              //2. SDNN - standard deviation for RR distances
+    rmssd = calculateRMSSD(nnIntervals);            //3. RMSSD - root mean sqare from RR intervals distance
+    nn50 = calculateNN50(nnIntervals);              //4. NN50  - Pair of neighbours RR with more than 50 distance
+    pnn50 = calculatepNN50(nnIntervals, nn50);      //5. pNN50 - percent of NN50 compared to all RR intervals
 
     timeParams_[0] = meanRR; 
     timeParams_[1] = sdnn;
@@ -43,27 +26,43 @@ void HRV_1::process(const Signal& rpeaks, const Signal& ecg){
 
 }
 
-// SDNN caltulator
-double HRV_1::calculateSDNN(const std::vector<double>& nnIntervals) const{
-    double meanNN =0.0;
-    double sum = 0.0;
-    
-    if (nnIntervals.empty()){return 0.0;}
+// get intervals from Rpeaks
+std::vector<double> HRV_1::getIntervals(const Signal& rpeaks) const{
+        std::vector<double> nnIntervals;
+        for (size_t i = 0; i < rpeaks.size() - 1; i++){
+        nnIntervals.push_back(rpeaks[i + 1] - rpeaks[i]);
+    }
+    return nnIntervals;
+}
 
-    for (double interval : nnIntervals){meanNN += interval;}
-    meanNN /= nnIntervals.size();
+// RR_mean calculator
+double HRV_1::calculateRRMean(const std::vector<double>& nnIntervals) const{
+    double sum = 0.0;
 
     for (double interval : nnIntervals){
-        double diff = interval - meanNN;
+        sum += interval;
+    }
+    return sum/nnIntervals.size();
+    }
+
+
+// SDNN caltulator
+double HRV_1::calculateSDNN(const std::vector<double>& nnIntervals) const{
+    double sum = 0.0;
+    double meanRR = calculateRRMean(nnIntervals);
+    if (nnIntervals.empty()){return 0.0;}
+
+    for (double interval : nnIntervals){
+        double diff = interval - meanRR;
         sum += diff * diff;
     }
 
     return std::sqrt(sum/nnIntervals.size());
 }
 
+ std::array<float, 5> timeParams_;
 // RMSSD calculator
 double HRV_1::calculateRMSSD(const std::vector<double>& nnIntervals) const{
-    double meanNN =0.0;
     double sum = 0.0;
     
     if (nnIntervals.empty()){return 0.0;}
@@ -93,7 +92,7 @@ double HRV_1::calculateNN50(const std::vector<double>& nnIntervals) const{
 double HRV_1::calculatepNN50(const std::vector<double>& nnIntervals, double nn50) const{return (nn50/nnIntervals.size())*100;}
 
 // HRV 1 getting time parameters
-std::array<float, 5> HRV_1::getTimeParams() const{return timeParams_;}
+std::array<double, 5> HRV_1::getTimeParams() const{return timeParams_;}
 
 // HRV 1 getting frequency paramters
-std::array<float, 6>  HRV_1::getFreqParams() const{return freqParams_;}
+std::array<double, 6>  HRV_1::getFreqParams() const{return freqParams_;}
