f3f450249686953ba2486bb66c883a1a931bc14a
Faelivrins <wika3252@gmail.com>
2024-12-10 21:50:45 +0100

    Added implementations of MovingMean and Butterworth + 1D convolution, needs to be adjusted to given architecture

--- include/Model/Baseline/MovingMeanFilter.h
+++ include/Model/Baseline/MovingMeanFilter.h
@@ -1,7 +1,22 @@
 #pragma once
 #include "Filter.h"
 
+//Will take single samples, which will be fed to the filter in loop in main component/program
+
 class MovingMeanFilter : public Filter {
-public:
-    Signal applyFilter(const Signal& inputSignal) const override;
+    public:
+        MovingMeanFilter(int filterLength);
+        double addSample(double newVal);
+        double getValue();
+        void dumpFilter();
+
+    private:
+        double *filterPtr;
+        int filterLength;
+        double lastVal;
+
+        void initFilter();
+        void shiftFilter(double nextVal);
+        void computeMean();
+
 };
