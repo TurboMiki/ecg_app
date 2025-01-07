03e2e88ed61999c307f28f042e85cab0bb72aabe
Faelivrins <wika3252@gmail.com>
2025-01-08 00:15:23 +0100

    Added rough implementation of LMS filtering

--- src/Model/Baseline/LMSFilter.cpp
+++ src/Model/Baseline/LMSFilter.cpp
@@ -1,6 +1,26 @@
 #include "LMSFilter.h"
+#include <LMS/AdaptiveFilter.c>
 
-Signal LMSFilter::applyFilter(const Signal& inputSignal) const {
+static AfData Adata = {
+		STEPSIZE,
+        REGULARIZATION,
+		NUM_TAPS,
+		inBuffer,
+        0, /* initial buffer index */
+		weights,
+		0.0 /* initial error */
+};
 
-    return inputSignal;
+Signal LMSFilter::applyFilter(const Signal& inputSignal, const Signal& refSignal) const {
+    //refSignal -> pewnie jakiś losowy z tej bazy(lub przefiltrowany w matlabie i zapisany z powrotem do tego .dat(?))
+    std::vector<double> oldY = inputSignal.getY;
+    std::vector<double> refY = refSignal.getY;
+    std::vector<double> newY;
+    
+    
+    for(i=oldY.begin(); i != oldY.end(); i++) {
+        newY.push_back(AdaptiveFilterRun(oldY[i], refY[i], &Adata));
+    }
+
+    inputSignal::SetY(newY);
 }
