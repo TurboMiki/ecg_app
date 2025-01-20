1ae30223ae3350339ac15b2dece9cbd9e69c256b
Oliwfka <166206701+Oliwfka@users.noreply.github.com>
2025-01-20 11:28:22 +0100

    Unit swap

--- src/Model/HRV_1/RRIntervals.cpp
+++ src/Model/HRV_1/RRIntervals.cpp
@@ -7,6 +7,11 @@ RRIntervals::RRIntervals(const Signal& rpeaks) {
 
 void RRIntervals::calculateIntervals(const Signal& rpeaks){
         std::vector<double> x = rpeaks.getX();
+
+        for (size_t i = 0; i < x.size(); i++) {
+            x[i] *= 1000; // Unit swap
+        }
+
         for (size_t i = 0; i < x.size() - 1; ++i) {
             nnIntervals_.push_back(x[i + 1] - x[i]);
         }
